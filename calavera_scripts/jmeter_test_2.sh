a7e56a8ca6f5b6eb8017746ba00077687368fb83
Marc Gracia <mgbuddy@gmail.com>
2015-09-17 01:03:25 +0200

    Added jmeter_test scripts with grafana

---
+++ calavera_scripts/jmeter_test_2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "### Running JMeter performance test ###"
+
+# Clear out old results
+rm $WORKSPACE/jmeter.jtl
+
+# Run the tests
+echo "## Running the tests"
+cd "$WORKSPACE/jmeter"
+
+START=`date +%s`
+jmeter -n -t jmeter_formdevops3.jmx -l $WORKSPACE/jmeter.jtl 
+END=`date +%s`
+curl -o grafana.png 'http://formdevops1.upcnet.es:8022/graphite/render?target=stats.gauges.biz.calavera.brazos.cpu.*&from='"$START"'&until='"$END"'&format=png&maxDataPoints=1636&width=1588&height=121&bgcolor=1f1f1f&fgcolor=BBBFC2&lineWidth=1&hideLegend=false&yUnitSystem=si&lineMode=connected'
