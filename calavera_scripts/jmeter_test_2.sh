710e079ccd3daa002db5f4443c6912ba88720690
Marc Gracia <mgbuddy@gmail.com>
2015-09-17 01:20:45 +0200

    Better way to publish grafs

--- calavera_scripts/jmeter_test_2.sh
+++ calavera_scripts/jmeter_test_2.sh
@@ -11,4 +11,6 @@ cd "$WORKSPACE/jmeter"
 START=`date +%s`
 jmeter -n -t jmeter_formdevops3.jmx -l $WORKSPACE/jmeter.jtl 
 END=`date +%s`
-curl -o grafana.png 'http://formdevops1.upcnet.es:8022/graphite/render?target=stats.gauges.biz.calavera.brazos.cpu.*&from='"$START"'&until='"$END"'&format=png&maxDataPoints=1636&width=1588&height=121&bgcolor=1f1f1f&fgcolor=BBBFC2&lineWidth=1&hideLegend=false&yUnitSystem=si&lineMode=connected'
+mkdir grafana
+cd grafana
+echo '<HTML><BODY><img src="http://formdevops1.upcnet.es:8022/graphite/render?target=stats.gauges.biz.calavera.brazos.cpu.*&from='"$START"'&until='"$END"'&format=png&maxDataPoints=1636&width=1588&height=121&bgcolor=1f1f1f&fgcolor=BBBFC2&lineWidth=1&hideLegend=false&yUnitSystem=si&lineMode=connected"></img></body></HTML>' > page1.html
