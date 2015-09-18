c6f4e07d466735f8b747f2bcca25b1053399d30a
Marc Gracia <mgbuddy@gmail.com>
2015-09-18 10:38:11 +0200

    Added deploy signal

---
+++ calavera_scripts/deploy_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /var/lib/tomcat6/webapps/ROOT/WEB-INF/lib/
+sudo rm CalaveraMain.*
+sudo wget "http://espina:8081/artifactory/simple/ext-release-local/Calavera/target/CalaveraMain.jar" -O CalaveraMain.jar
+cd /var/lib/tomcat6/webapps/ROOT/WEB-INF/
+sudo rm web.xml*
+sudo wget "http://espina:8081/artifactory/simple/ext-release-local/Calavera/target/web.xml" -O web.xml
+sudo /etc/init.d/tomcat6 restart
+echo "biz.calavera.hombros.deploy.hijo:1|c"|nc -w 1 -u grafana 8125
