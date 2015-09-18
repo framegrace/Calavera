cb583a090b021b59761b09332ec06cf7b042b36a
Marc Gracia <mgbuddy@gmail.com>
2015-09-18 10:30:02 +0200

    Timing on tests

--- calavera_scripts/jmeter_test_2.sh
+++ calavera_scripts/jmeter_test_2.sh
@@ -11,6 +11,4 @@ cd "$WORKSPACE/jmeter"
 START=`date +%s`
 jmeter -n -t jmeter_formdevops3.jmx -l $WORKSPACE/jmeter.jtl 
 END=`date +%s`
-mkdir grafana
-cd grafana
-echo '<HTML><BODY><img src="http://formdevops1.upcnet.es:8022/graphite/render?target=stats.gauges.biz.calavera.brazos.cpu.*&from='"$START"'&until='"$END"'&format=png&maxDataPoints=1636&width=1588&height=121&bgcolor=1f1f1f&fgcolor=BBBFC2&lineWidth=1&hideLegend=false&yUnitSystem=si&lineMode=connected"></img></body></HTML>' > page1.html
+echo "biz.calavera.hombros.tests.hijo.time:$(($END-$START))|ms"|nc -w 1 -u grafana 8125
