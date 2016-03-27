1471c23f3c5ae29ee050903a843f39b174b3e6c1
Marc <mgbuddy@gmail.com>
2016-03-27 08:53:22 +0000

    Prepare to be executed from any directory

--- calavera.startup.sh
+++ calavera.startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BASE=`dirname $0`
+pushd $BASE
 NIC="eth0"
 name="dnsmasq"
 export MY_IP=$(ifconfig $NIC | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
@@ -23,3 +24,4 @@ do
     echo "${C_IP} ${NODE} ${NODE}.calavera.biz" >> dnsmasq.hosts/calavera
     docker kill -s HUP dnsmasq
 done
+popd
